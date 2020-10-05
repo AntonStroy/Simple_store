2583269d431d8d0bacc960aee9e2f86f2f60fbad
Anton Stroy <antosha.stroy@gmail.com>
2020-10-04 23:53:05 -0500

    Updaeted categories and products from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-767.times do
-  Product.create(title:          Faker::Commerce.product_name,
-                 price:          Faker::Number.decimal(l_digits: 2),
-                 stock_quantity: Faker::Number.between(from: 1, to: 500))
+# 767.times do
+#   Product.create(title:          Faker::Commerce.product_name,
+#                  price:          Faker::Number.decimal(l_digits: 2),
+#                  stock_quantity: Faker::Number.between(from: 1, to: 500))
+# end
+
+require "csv"
+Product.destroy_all
+Category.destroy_all
+
+csvFile = Rails.root.join("db/products.csv")
+csvData = File.read(csvFile)
+
+products = CSV.parse(csvData, headers: true)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product["category"])
+  if category.valid?
+    newProduct = category.products.create(title:          product["name"],
+                                          price:          product["price"],
+                                          description:    product["description"],
+                                          stock_quantity: product["stock quantity"])
+    puts "Fail to create product"
+  else
+    puts "Fail to create category"
+  end
 end
+
+puts "Successfuly Created #{Category.count} categories"
+puts "Successfuly Created #{Product.count} products"
